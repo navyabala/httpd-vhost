93bead55a25b835fcf1c267ca9882e4c709c9d3b
rajeswari1203 <rajeswaribadeti@gmail.com>
2017-11-07 13:43:48 +0000

    second

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures httpd'
 long_description 'Installs/Configures httpd'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
+depends 'httpd'
 depends 'hostsfile', '= 2.4.5'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
