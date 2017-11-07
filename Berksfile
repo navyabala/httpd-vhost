d5e6c4b905fe36e16237400ea1f8f18be9de3099
rajeswari1203 <rajeswaribadeti@gmail.com>
2017-11-07 07:18:50 +0000

    first commit

---
+++ Berksfile
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+source 'https://supermarket.chef.io'
+#cookbook 'httpd', path: './cookbooks/httpd'
+metadata
+cookbook "hostsfile", "~> 2.4.5"
---
+++ recipes/httpd_install.rb
@@ -0,0 +1,43 @@
+# add hosts entry
+hostsfile_entry '127.0.0.1' do
+	hostname  'tryenbd'
+	action    :create
+end
+
+# create directory
+directory '/var/www/shop' do
+	owner 'root'
+	group 'root'
+	mode '0775'
+	recursive true
+	action :create
+end
+
+# create virtual host
+web_app 'shop' do
+	server_name 'tryenbd'
+	server_aliases ['tryenbd']
+	docroot '/var/www/shop'
+	cookbook 'apache2'
+end
+
+# create dummy content
+execute "create dummy site" do
+	command "echo 'hello' > /var/www/shop/index.html"
+	cwd "/var/www/shop"
+	action :run
+end
+
+# fix owner
+execute "root" do
+	command "chown -R root:root ."
+	cwd "/var/www/shop"
+	action :run
+end
+
+# fix permission
+execute "root" do
+	command "chmod -R 775 ."
+	cwd "/var/www/shop"
+	action :run
+end
